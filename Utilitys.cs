336eed30dd18bdb8cf55e00127a1181958d84d0c
Travis-DV <tfindley2@my.stlcc.edu>
2024-06-29 15:18:33 -0500

    Added Code

---
+++ Utilitys.cs
@@ -0,0 +1,40 @@
+
+namespace Utilitys
+{
+    public static class RandomNumber
+    {
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+        public static int Between(int minimumValue, int maximumValue)
+        {
+            if (minimumValue == 0 && maximumValue == 0) { return 0; }
+            byte[] randomNumbers = new byte[1];
+            _generator.GetBytes(randomNumbers);
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumbers[0]);
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+            int range = (maximumValue - 1) - minimumValue;
+            double randomValueInRange = Math.Floor(multiplier * range);
+            console.Log($"method; (RandomNumber.Between), Between; (min: {minimumValue}, max: {maximumValue}), Value; ({(int)(minimumValue + randomValueInRange)})");
+            return (int)(minimumValue + randomValueInRange);
+        }
+    }
+
+    public static class Extensions
+    {
+        public static T pop<T>(this List<T> list, int index = -1)
+        {
+            if (list.Count == 0) { throw new InvalidOperationException("Cannot pop from an empty list"); }
+
+            if (index == -1) { index = list.Count - 1; }
+
+            T temp = list[index];
+            list.RemoveAt(index);
+            console.Log($"method; (Extensions.pop), Thing Popped; ({temp})");
+            return temp;
+        }
+
+        public static int ToInt(this bool value)
+        {
+            return value ? 1 : 0;
+        }
+    }
+}
